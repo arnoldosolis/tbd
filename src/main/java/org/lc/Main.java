01526ddcfbdf1fc6d7c1a7f5ac40c3537d926804
Arnoldo Solis <arnsol354@gmail.com>
2025-06-03 14:59:49 -0400

    first commit

---
+++ src/main/java/org/lc/Main.java
@@ -0,0 +1,13 @@
+package org.lc;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
+        System.out.println(Arrays.toString(productOfArrayExceptSelf.solution1(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(productOfArrayExceptSelf.solution2(new int[]{1, 2, 3, 4})));
+    }
+}
---
+++ src/main/java/org/lc/ProductOfArrayExceptSelf.java
@@ -0,0 +1,41 @@
+package org.lc;
+
+public class ProductOfArrayExceptSelf {
+
+    // Brute Force - Time O(N^2) - Space is O(N)
+    public int[] solution1(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+            result[i] = product;
+        }
+        return result;
+    }
+
+    // Best Solution - Time O(N) - Space is O(N)
+    public int[] solution2(int[] nums) {
+        int n = nums.length;
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
+        }
+        return ans;
+    }
+}
